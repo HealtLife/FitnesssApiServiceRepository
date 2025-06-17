eb3a3f8e37c7c0d74f39d0d9f4046f4996fe4252
Nato0211 <nato0211@gmail.com>
2025-06-17 18:17:52 -0500

    feat created FitnessApiService

---
+++ src/main/java/com/healthlife/u20221b471/main/fitnessapiservice/FitnessApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.healthlife.u20221b471.main.fitnessapiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessApiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/healthlife/u20221b471/main/fitnessapiservice/FitnessApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthlife.u20221b471.main.fitnessapiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
